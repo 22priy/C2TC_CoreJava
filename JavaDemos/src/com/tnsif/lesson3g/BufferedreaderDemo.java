58f468bab7de23247350bd07566174b660988d12
HP <HP@DESKTOP-HVHQH6M>
2022-08-25 21:15:55 +0530

    commit

---
+++ JavaDemos/src/com/tnsif/lesson3g/BufferedreaderDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.lesson3g;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+import java.io.IOException;
+
+public class BufferedreaderDemo {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+BufferedReader obj = new BufferedReader(new  InputStreamReader(System.in));
+System.out.println("Enter your name");
+String firstName = obj.readLine();
+System.out.println(firstName);
+
+
+
+	}
+
+}
